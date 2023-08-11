0a833d92be7ef109ef64489cb4c09b9db2b0d156
MikeMalindzisa <machaweml@gmail.com>
2023-08-11 07:01:46 -0700

    103-fast_alphabet.py

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-import string; (lambda s: [__import__('sys').stdout.write(s[i]) for i in range(len(s))])(string.ascii_uppercase + '\n')
+import string; print(''.join(list(map(chr, range(ord('A'), ord('Z')+1)))) + '\n')
