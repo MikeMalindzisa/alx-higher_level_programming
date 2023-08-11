21102e598e05c0b678181467df74d9872d44c082
MikeMalindzisa <machaweml@gmail.com>
2023-08-11 04:17:29 -0700

    Fix for excecution even when used __import__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
 
 a = 1
 b = 2
 
-result = add(a, b)
+from add_0 import add
 
-print("{:d} + {:d} = {:d}".format(a, b, result))
+if __name__ == "__main__":
+    result = add(a, b)
+    print(f"{a} + {b} = {result}")
