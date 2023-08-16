b40afe7bc24a98e0eeb9f60543e783ce18a8610e
MikeMalindzisa <machaweml@gmail.com>
2023-08-16 16:46:14 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,4 +3,5 @@ def roman_to_int(s):
     if not isinstance(s, str) or not s:
         return 0
     r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    return sum((r[s[i]], -r[s[i]])[r[s[i]] < r[s[i+1]]] for i in range(len(s) - 1)) + r[s[-1]]
+    result = sum((r[s[i]], -r[s[i]])[r[s[i]] < r[s[i+1]]] for i in range(len(s) - 1)) + r[s[-1]]
+    return result if 1 <= result <= 3999 else 0
