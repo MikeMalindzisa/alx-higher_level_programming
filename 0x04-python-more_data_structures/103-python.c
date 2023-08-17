73a37bdbe746264c49db124b90fd5d7414111ccb
MikeMalindzisa <machaweml@gmail.com>
2023-08-16 17:26:10 -0700

    103-python.c

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,43 @@
+#include <Python.h>
+
+void print_python_list(PyObject *p)
+{
+    Py_ssize_t size = PyList_Size(p);
+    Py_ssize_t allocated = ((PyListObject *)p)->allocated;
+
+    printf("[*] Python list info\n");
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", allocated);
+
+    for (Py_ssize_t i = 0; i < size; ++i)
+    {
+        PyObject *element = PyList_GetItem(p, i);
+        const char *type = Py_TYPE(element)->tp_name;
+
+        printf("Element %ld: %s\n", i, type);
+    }
+}
+
+void print_python_bytes(PyObject *p)
+{
+    printf("[.] bytes object info\n");
+
+    if (!PyBytes_Check(p))
+    {
+        printf("  [ERROR] Invalid Bytes Object\n");
+        return;
+    }
+
+    Py_ssize_t size = PyBytes_Size(p);
+    printf("  size: %ld\n", size);
+
+    char *str = PyBytes_AsString(p);
+    printf("  trying string: %s\n", str);
+
+    printf("  first %ld bytes:", size < 10 ? size : 10);
+    for (Py_ssize_t i = 0; i < size && i < 10; ++i)
+    {
+        printf(" %02x", (unsigned char)str[i]);
+    }
+    printf("\n");
+}
