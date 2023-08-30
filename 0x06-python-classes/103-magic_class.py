ae825f27477150b28abec4aec76ee098a35db285
MikeMalindzisa <machaweml@gmail.com>
2023-08-30 16:09:05 -0700

    103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,45 +1,29 @@
 #!/usr/bin/python3
+import dis
 import math
 
+class _MagicClass__radius:
+    def __init__(self, radius):
+        self._MagicClass__radius = 0
 
-class MagicClass:
-    """
-    A class that represents a magic circle with radius-related calculations.
-
-    Attributes:
-        radius (int or float): The radius of the magic circle.
-
-    Methods:
-        __init__(self, radius=0): Initializes a new MagicClass instance.
-        area(self): Calculates the area of the magic circle.
-        circumference(self): Calculates the circumference of the magic circle.
-    """
-    def __init__(self, radius=0):
-        """
-        Initializes a new MagicClass instance.
-
-        Args:
-            radius (int or float): The radius of the magic circle.
-        """
-        self.__radius = 0
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        self.__radius = radius
+
+        self._MagicClass__radius = radius
 
     def area(self):
-        """
-        Calculates the area of the magic circle.
-
-        Returns:
-            float: The area of the magic circle.
-        """
-        return self.__radius ** 2 * math.pi
+        return self._MagicClass__radius ** 2 * math.pi
 
     def circumference(self):
-        """
-        Calculates the circumference of the magic circle.
+        return 2 * math.pi * self._MagicClass__radius
 
-        Returns:
-            float: The circumference of the magic circle.
-        """
-        return 2 * math.pi * self.__radius
+magic_instance = _MagicClass__radius(5)
+
+print("Disassembly of __init__:")
+dis.dis(magic_instance.__init__)
+
+print("\nDisassembly of area:")
+dis.dis(magic_instance.area)
+
+print("\nDisassembly of circumference:")
+dis.dis(magic_instance.circumference)
