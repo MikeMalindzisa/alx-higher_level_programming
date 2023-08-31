3d47aee6abb1634b75ae23984041374be31b8a72
MikeMalindzisa <machaweml@gmail.com>
2023-08-30 18:36:43 -0700

    102-magic_calculation.py

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,17 @@
+import dis
+
+
+def calculate_result(a, b):
+    result = 0
+
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            result += (a ** b) / i
+        except:
+            result += a + b
+
+    return result
+
+dis.dis(calculate_result)
