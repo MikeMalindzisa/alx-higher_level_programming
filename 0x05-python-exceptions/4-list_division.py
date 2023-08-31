a91f3e021070e379b63fdc30b15f2f530aa72365
MikeMalindzisa <machaweml@gmail.com>
2023-08-30 17:42:12 -0700

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,34 +3,40 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     """
-    Divides element by element two lists and returns a new list with
-    results.
+    Divides element by element from two lists and returns a new list
+    with the divisions.
 
     Args:
         my_list_1 (list): The first list.
         my_list_2 (list): The second list.
-        list_length (int): The length of the new list to be returned.
+        list_length (int): The desired length of the resulting list.
 
     Returns:
-        list: A new list with division results.
+        list: A new list containing the division results.
     """
     result = []
+
     for i in range(list_length):
         try:
-            num_1 = my_list_1[i] if i < len(my_list_1) else 0
-            num_2 = my_list_2[i] if i < len(my_list_2) else 0
+            div = 0
+            if i < len(my_list_1) and i < len(my_list_2):
+                element1 = my_list_1[i]
+                element2 = my_list_2[i]
 
-            if (isinstance(num_1, (int, float)) and
-                    isinstance(num_2, (int, float))):
-                if num_2 != 0:
-                    result.append(num_1 / num_2)
+                if isinstance(element1, (int, float)) and \
+                   isinstance(element2, (int, float)):
+                    if element2 != 0:
+                        div = element1 / element2
+                    else:
+                        print("division by 0")
                 else:
-                    result.append(0)
-                    print("division by 0")
+                    print("wrong type")
             else:
-                result.append(0)
-                print("wrong type")
-        except IndexError:
+                print("out of range")
+            result.append(div)
+        except (IndexError, ZeroDivisionError):
             result.append(0)
-            print("out of range")
+        except TypeError:
+            print("wrong type")
+
     return result
