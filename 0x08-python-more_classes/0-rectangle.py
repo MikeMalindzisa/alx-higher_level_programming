b10d28694f3844f2b126087da297f4eb916de190
MikeMalindzisa <machaweml@gmail.com>
2023-09-05 09:14:49 -0700

    0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,4 +2,13 @@
 
 
 class Rectangle:
-        pass
+    """
+    This class represents a simple rectangle.
+    """
+
+
+def __init__(self):
+    """
+    Initializes a new instance of the Rectangle class.
+    """
+    pass                        
