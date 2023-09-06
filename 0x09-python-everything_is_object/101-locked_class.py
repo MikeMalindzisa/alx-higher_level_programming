7fc0cd998537daaf36537cef9495d9b916e17571
MikeMalindzisa <machaweml@gmail.com>
2023-09-05 19:22:25 -0700

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,28 @@
+class LockedClass:
+    """
+    A class that restricts the creation of instance attributes, allowing only 'first_name' to be set.
+    
+    Attributes:
+        None
+
+    Methods:
+        __setattr__(self, attr, value):
+            Overrides the default attribute setter method to control attribute creation.
+            Raises an AttributeError if the attribute name is not 'first_name'.
+    """
+
+    def __setattr__(self, attr, value):
+        """
+        Override the default attribute setter method to control attribute creation.
+        
+        Args:
+            attr (str): The name of the attribute to be set.
+            value (any): The value to assign to the attribute.
+        
+        Raises:
+            AttributeError: If the attribute name is not 'first_name'.
+       """
+        if attr != "first_name":
+            raise AttributeError("'LockedClass' object has no attribute '{}'".format(attr))
+        super().__setattr__(attr, value)
+
