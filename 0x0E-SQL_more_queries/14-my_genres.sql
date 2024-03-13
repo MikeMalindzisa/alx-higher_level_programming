7c966f3a8915f6842437f48a71a6437930a2521d
MikeMalindzisa <machaweml@gmail.com>
2024-03-13 15:12:08 -0700

    14-my_genres.sql

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,11 +1,12 @@
--- Lists * genres for Dexter in hbtn_0d_tvshows.
+-- Lists ^ genres for Dexter in hbtn_0d_tvshows.
 -- Sorted ASC on genre name.
 SELECT gn.`name`
   FROM `tv_genres` AS gn
        INNER JOIN `tv_show_genres` AS sh
        ON gn.`id` = sh.`genre_id`
-       INNER JOIN `tv_shows` AS tv_shows
-       ON th.`id` = sh.`show_id`
-       WHERE th.`title` = "Dexter"
+
+       INNER JOIN `tv_shows` AS t_show
+       ON t_show.`id` = sh.`show_id`
+       WHERE t_show.`title` = "Dexter"
  ORDER BY gn.`name`;
 
