28402d46200ef60b370e20fa91ecc15f41f2aeff
MikeMalindzisa <machaweml@gmail.com>
2024-04-08 16:45:24 -0700

    semmistandard fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,23 +1,22 @@
 #!/usr/bin/node
-function secondBiggest(args) {
-    if (args.length <= 2) {
-        return 0;
+function secondBiggest (args) {
+  if (args.length <= 2) {
+    return 0;
+  }
+  let max = Number.MIN_SAFE_INTEGER;
+  let secondMax = Number.MIN_SAFE_INTEGER;
+
+  for (let i = 2; i < args.length; i++) {
+    const num = parseInt(args[i]);
+    if (num > max) {
+      secondMax = max;
+      max = num;
+    } else if (num > secondMax && num !== max) {
+      secondMax = num;
     }
+  }
 
-    let max = Number.MIN_SAFE_INTEGER;
-    let secondMax = Number.MIN_SAFE_INTEGER;
-
-    for (let i = 2; i < args.length; i++) {
-        const num = parseInt(args[i]);
-        if (num > max) {
-            secondMax = max;
-            max = num;
-        } else if (num > secondMax && num !== max) {
-            secondMax = num;
-        }
-    }
-
-    return secondMax;
+  return secondMax;
 }
 
 const args = process.argv;
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,13 +4,13 @@ const arg = process.argv[2];
 const size = parseInt(arg);
 
 if (isNaN(size)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let i = 0; i < size; i++) {
-        let row = '';
-        for (let j = 0; j < size; j++) {
-            row += 'X';
-        }
-        console.log(row);
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
     }
+    console.log(row);
+  }
 }
