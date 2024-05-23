17358da95fd72b1379bd8a5b45c061cf5dd90d37
MikeMalindzisa <machaweml@gmail.com>
2024-05-22 18:56:29 -0700

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -8,16 +8,15 @@ const fileName = process.argv[3];
 const fileStream = fs.createWriteStream(fileName);
 
 fileStream.on('error', (err) => {
-  console.error(`Error writing to ${fileName}: ${err}`);
+ 	fileStream.close();
 });
 
 request(endpoint)
   .on('error', (err) => {
-    console.error(`Error downloading from ${endpoint}: ${err}`);
     fileStream.close();
     fs.unlinkSync(fileName);
   })
   .pipe(fileStream)
   .on('finish', () => {
-    console.log(`File downloaded successfully as ${fileName}`);
+    fileStream.close();
   });
