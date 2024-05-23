ef1834d86e197d0e22d29d4cbf8bf5309387d3a9
MikeMalindzisa <machaweml@gmail.com>
2024-05-22 18:45:44 -0700

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,6 @@ const request = require('request');
 
 const endpoint = process.argv[2];
 
-request.get(endpoint).on('res', function (response) {
+request.get(endpoint).on('response', function (res) {
   console.log(`code: ${res.statusCode}`);
 });
