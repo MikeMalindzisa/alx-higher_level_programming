300eb0a571ad0856f414ba727b8650a9318b5335
MikeMalindzisa <machaweml@gmail.com>
2024-05-22 18:49:10 -0700

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  
+  let count = 0;
+  const data = JSON.parse(body);
+  for (const result of data.results) {
+    if (result.characters.some(url => url.includes('18'))) {
+      count++;
+    }
+  }
+  
+  console.log(count);
+});
